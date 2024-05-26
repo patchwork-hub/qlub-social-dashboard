811734596e66146848fdbb06a947d357c2db241f
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-05-26 16:33:02 +0700

    Update keyword_filter_api_service.rb

--- app/services/keyword_filter_api_service.rb
+++ app/services/keyword_filter_api_service.rb
@@ -2,8 +2,8 @@ class KeywordFilterApiService
   require 'httparty'
   
   def initialize
-      @base_url = Rails.env.production? ? "https://hub.patchwork.online/api/v1/keyword_filters" : ENV['CENTRAL_DASHBOARD_URL']
-      @api_key = Rails.env.production? ? "8e225f965e51445fd5e27c5870111481" : ENV['CENTRAL_DASHBOARD_KEY']
+      @base_url = "https://hub.patchwork.online/api/v1/keyword_filters"
+      @api_key = "8e225f965e51445fd5e27c5870111481"
   end
   
   def get_keywords_filters
