30cfcbae831219983bdcee47b3df315930a8366d
SithuBo <sithubo.stb97@gmail.com>
2024-06-12 10:54:17 +0630

    [add]dependent destroy to keyword filter group

--- app/models/server_setting.rb
+++ app/models/server_setting.rb
@@ -3,7 +3,7 @@ class ServerSetting < ApplicationRecord
 
   has_many :user_server_settings
   has_many :users, through: :user_server_settings
-  has_many :keyword_filter_groups
+  has_many :keyword_filter_groups, dependent: :destroy
 
   belongs_to :parent, class_name: "ServerSetting", optional: true
   has_many :children, class_name: "ServerSetting", foreign_key: "parent_id", dependent: :destroy
