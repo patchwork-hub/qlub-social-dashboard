7ee93239444682d7150b3a3b66049fbbc5121f7f
minmaung132h <minmaung132h@prestigein.com>
2024-07-16 17:55:04 +0700

    fixed communities_controller.rb

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -87,30 +87,30 @@ class CommunitiesController < BaseController
   end
 
   def create
-    @community = CommunityService.new.call(@account, 
-                                  name: @options[:name], 
-                                  slug: @options[:slug],
-                                  description: @options[:description],
-                                  is_recommended: @options[:is_recommended],
-                                  bio:  @options[:bio],
-                                  guides: @options[:guides],
-                                  patchwork_collection_id: @collection.id,
-                                  position: @options[:position],
-                                  admin_following_count: @options[:admin_following_count],
-                                  image: @options[:image])
+    # @community = CommunityService.new.call(@account, 
+    #                               name: @options[:name], 
+    #                               slug: @options[:slug],
+    #                               description: @options[:description],
+    #                               is_recommended: @options[:is_recommended],
+    #                               bio:  @options[:bio],
+    #                               guides: @options[:guides],
+    #                               patchwork_collection_id: @collection.id,
+    #                               position: @options[:position],
+    #                               admin_following_count: @options[:admin_following_count],
+    #                               image: @options[:image])
   end
 
   def community_params
-    params.require(:community).permit(
-      :name,
-      :slug,
-      :description,
-      :collection_id,
-      :position,
-      :is_recommended,
-      :bio,
-      :image,
-      guides:[:position,:title,:description])
+    # params.require(:community).permit(
+    #   :name,
+    #   :slug,
+    #   :description,
+    #   :collection_id,
+    #   :position,
+    #   :is_recommended,
+    #   :bio,
+    #   :image,
+    #   guides:[:position,:title,:description])
   end
 
   private
