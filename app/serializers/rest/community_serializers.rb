8628cd5474744123554285cdaa139e61c1e570dc
minmaung132h <minmaung132h@prestigein.com>
2024-07-16 17:30:53 +0700

    fixed community_serializers.rb

--- app/serializers/rest/community_serializers.rb
+++ app/serializers/rest/community_serializers.rb
@@ -1,3 +1,2 @@
-class REST::CommunitySerializer < ActiveModel::Serializer
-    attributes :id, :name, :slug, :description, :is_recommended, :admin_following_count, :account_id, :created_at, :updated_at, :patchwork_collection_id, :position, :guides, :participants_count
+class REST::CommunitySerializers < ActiveModel::Serializer
 end
