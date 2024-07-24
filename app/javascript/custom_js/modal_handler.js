9b7042348a734c6774ee6b9de7bddfbc73b4fc37
SithuBo <sithubo.stb97@gmail.com>
2024-07-24 10:21:15 +0630

    [fix] jquery

--- app/javascript/custom_js/modal_handler.js
+++ app/javascript/custom_js/modal_handler.js
@@ -1,38 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function setModalDetails(title, id, label) {
-    // Set the modal title
-    $('.modal-title').text(title);
-    // Set the hidden input for server setting ID
-    const serverSettingIdInput = $('#serverSettingId');
-    serverSettingIdInput.val(id);
-    // Set the name label text
-    $('label[for="keyword_filter_group_name"]').text(label);
-    // Fetch and populate existing data for the select element
-    fetchExistingData(id);
-  }
 
-  function fetchExistingData(serverSettingId) {
-    $.ajax({
-      url: '/server_settings/group_data',
-      data: { server_setting_id: serverSettingId },
-      success: function(data) {
-        const selectElement = $('#keyword_filter_group_name');
-        selectElement.empty();
-        data.forEach(function(item) {
-          selectElement.append(new Option(item, item));
-        });
-      },
-      error: function(xhr, status, error) {
-        console.error('Error fetching data:', error);
-      }
-    });
-  }
+function setModalDetails(title, id, label) {
+  // Set the modal title
+  $('.modal-title').text(title);
+  // Set the hidden input for server setting ID
+  const serverSettingIdInput = $('#serverSettingId');
+  serverSettingIdInput.val(id);
+  // Set the name label text
+  $('label[for="keyword_filter_group_name"]').text(label);
+  // Fetch and populate existing data for the select element
+  fetchExistingData(id);
+}
 
-  $('a[data-toggle="modal"]').click(function (event) {
-    event.preventDefault();
-    const title = $(this).attr('title');
-    const id = $(this).data('id');
-    const label = $(this).data('label');
-    setModalDetails(title, id, label);
+function fetchExistingData(serverSettingId) {
+  $.ajax({
+    url: '/server_settings/group_data',
+    data: { server_setting_id: serverSettingId },
+    success: function(data) {
+      const selectElement = $('#keyword_filter_group_name');
+      selectElement.empty();
+      data.forEach(function(item) {
+        selectElement.append(new Option(item, item));
+      });
+    },
+    error: function(error) {
+      console.error('Error fetching data:', error);
+    }
   });
+}
+
+$('a[data-toggle="modal"]').click(function (event) {
+  event.preventDefault();
+  const title = $(this).attr('title');
+  const id = $(this).data('id');
+  const label = $(this).data('label');
+  setModalDetails(title, id, label);
 });
