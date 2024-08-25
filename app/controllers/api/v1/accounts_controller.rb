a67c600b414226cf75a9faf04b125e6227747195
Zin Moe <dev.zinmoe@gmail.com>
2024-08-25 13:39:07 +0630

    add fediverse users in Account api

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -3,14 +3,15 @@ module Api
     class AccountsController < ApiController
 
       def index
-        @acounts = Account.includes(user: :role)
-                          .where.not(user_roles: {name: ['Moderator', 'Admin', 'Owner']})
-                          .page(params[:page] || 1)
-                          .per(params[:per_page] || 50)
+        @accounts = Account.joins("INNER JOIN users ON accounts.id = users.account_id")
+                           .joins("LEFT JOIN user_roles ON users.role_id = user_roles.id")
+                           .where("user_roles.name NOT IN (?) OR users.role_id IS NULL", ['Moderator', 'Admin', 'Owner'])
+                           .page(params[:page] || 1)
+                           .per(params[:per_page] || 50)
 
         options = {}
-        options[:meta] = get_metadata(@acounts)
-        render json: AccountSerializer.new(@acounts, options)
+        options[:meta] = get_metadata(@accounts)
+        render json: AccountSerializer.new(@accounts, options)
       end
 
     end
