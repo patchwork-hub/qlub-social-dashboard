5c493da1ea4b8a1919cfd1961c7d82271535b776
Zin Moe <dev.zinmoe@gmail.com>
2024-08-27 20:44:56 +0630

    update accounts query

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -6,12 +6,18 @@ module Api
         @accounts = Account.joins("INNER JOIN users ON accounts.id = users.account_id")
                            .joins("LEFT JOIN user_roles ON users.role_id = user_roles.id")
                            .where("user_roles.name NOT IN (?) OR users.role_id IS NULL", ['Moderator', 'Admin', 'Owner'])
-                           .page(params[:page] || 1)
-                           .per(params[:per_page] || 50)
+
+        @accounts = @accounts.where("accounts.created_at >= :date
+                                      OR accounts.suspended_at >= :date
+                                      OR users.confirmed_at >= :date",
+                                      date: params[:last_synced_at]
+                                    ) if params[:last_synced_at].present?
+
+        @accounts = @accounts.page(params[:page] || 1).per(params[:per_page] || 50)
 
         options = {}
         options[:meta] = get_metadata(@accounts)
-        render json: AccountSerializer.new(@accounts, options)
+        render json: Api::V1::AccountSerializer.new(@accounts, options)
       end
 
     end
