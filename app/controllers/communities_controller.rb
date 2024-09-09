f23c1f3eb41195a9b21a20c350fb0a7399b54c88
minmaung132h <minmaung132h@prestigein.com>
2024-09-09 13:29:10 +0700

    fixed communities_controller.rb

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -57,7 +57,6 @@ class CommunitiesController < BaseController
     @community = Community.find(session[:form_data]['id'])
     @contributor_records = load_contributors_records
     @contributor_search = commu_contributors_filter.build_search
-
     respond_to do |format|
       format.html { render partial: 'communities/contributors_table', locals: { records: @contributor_records } }
     end
@@ -69,6 +68,12 @@ class CommunitiesController < BaseController
 
     @community_hashtag_form = Form::CommunityHashtag.new
     @community = Community.find(session[:form_data]['id'])
+
+    @community_admin = CommunityAdmin.where(patchwork_community_id: session[:form_data]['id']).last.account_id
+
+    @follower_records = load_contributors_records
+    @follower_search = commu_contributors_filter.build_search
+    
     respond_to do |format|
       format.html
     end
@@ -84,6 +89,7 @@ class CommunitiesController < BaseController
   def step4
     @filter_keywords = get_community_filter_keyword
     admin_id = get_community_admin_id
+    @muted_accounts = get_muted_accounts
     @community_filter_keyword = CommunityFilterKeyword.new(
       patchwork_community_id: session[:form_data]['id'],
       account_id: admin_id
@@ -138,6 +144,8 @@ class CommunitiesController < BaseController
     @community = Community.find(session[:form_data]['id'])
     @rule_from = Form::CommunityRule.new
     @rule_records = CommunityRule.where(patchwork_community_id: @community.id)
+    @aditional_information = @community.patchwork_community_additional_informations
+    @community_admin = Account.find_by_id(get_community_admin_id)
   end
 
   def step6_rule_create
@@ -145,9 +153,13 @@ class CommunitiesController < BaseController
     redirect_to step6_communities_path
   end
 
-  def step6_save
-    respond_to do |format|
-      format.html
+  def set_visibility
+    @community = Community.find(session[:form_data]['id'])
+    if @community.update(visibility: params[:community][:visibility])
+      clear_form_data_id
+      redirect_to communities_path
+    else
+      render :step6
     end
   end
 
@@ -182,6 +194,49 @@ class CommunitiesController < BaseController
     render json: response.parsed_response
   end
 
+  def mute_contributor
+    target_account_id = params[:account_id]
+    admin_account_id = get_community_admin_id
+    mute = params[:mute]
+    
+    if mute
+      Mute.find_or_create_by!(account_id: admin_account_id, target_account_id: target_account_id, hide_notifications: true)
+    else
+      Mute.find_by(account_id: admin_account_id, target_account_id: target_account_id)&.destroy
+    end
+  
+    render json: { success: true }
+  end
+
+def unmute_contributor
+    target_account_id = params[:account_id]
+    admin_account_id = get_community_admin_id
+    Mute.find_by(account_id: admin_account_id, target_account_id: target_account_id)&.destroy
+  
+    redirect_to step4_communities_path
+  end
+
+  def is_muted
+    target_account_id = params[:account_id]
+    admin_account_id = get_community_admin_id
+    is_muted = Mute.exists?(account_id: admin_account_id, target_account_id: target_account_id)
+  
+    render json: { is_muted: is_muted }
+  end  
+
+  def manage_additional_information
+    @community = Community.find(session[:form_data]['id'])
+    if @community.update(community_params)
+      flash[:success] = "Additional information added successfully!"
+      redirect_to step6_communities_path
+    else
+      flash[:error] = "Something went wrong!"
+      step6 
+    
+      render :step6
+    end
+  end
+
   private
 
   def initialize_form
@@ -205,6 +260,12 @@ class CommunitiesController < BaseController
     params.require(:form_community_admin).permit(:community_id, :display_name, :username, :email, :password)
   end
 
+  def community_params
+    params.require(:community).permit(
+      patchwork_community_additional_informations_attributes: [:id, :heading, :text, :_destroy]
+    )
+  end
+
   def records_filter
     @filter = Filter::Community.new(params)
   end
@@ -223,6 +284,14 @@ class CommunitiesController < BaseController
 
   def load_post_hashtag_records
     post_hashtag_records_filter.get
+  end
+
+  def load_commu_follower_records
+    commu_follower_filter.get
+  end
+
+  def commu_follower_filter
+    @follower_filter = Filter::Account.new(params)
   end
 
   def commu_contributors_filter
@@ -252,9 +321,20 @@ class CommunitiesController < BaseController
     CommunityAdmin.where(patchwork_community_id: session[:form_data]['id']).last.account_id
   end
 
+  def get_muted_accounts
+    admin_account_id = get_community_admin_id
+    muted_account_ids = Mute.where(account_id: admin_account_id).pluck(:target_account_id)
+    Account.where(id: muted_account_ids)
+  end
+  
   def set_community
     @community = Patchwork::Community.find_by(slug: params[:id])
     raise ActiveRecord::RecordNotFound unless @community
+  end
+
+  def clear_form_data_id
+    session[:form_data] ||= {}
+    session[:form_data]['id'] = nil
   end
 
   def set_current_step
