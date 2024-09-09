673cdb3cadd91bada046a812cc6728d5ef9af690
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2024-09-09 22:10:00 +0630

    Fix enum keys

--- app/models/keyword_filter.rb
+++ app/models/keyword_filter.rb
@@ -2,6 +2,6 @@ class KeywordFilter < ApplicationRecord
   self.table_name = 'keyword_filters'
   validates :keyword, presence: true, uniqueness: true
 
-  enum filter_type: { "post content": 0, hashtags: 1, both: 2 }
+  enum filter_type: { content: 0, hashtag: 1, both: 2 }
 
 end
