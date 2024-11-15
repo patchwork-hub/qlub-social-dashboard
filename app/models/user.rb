1848df183cb30deb847c66decf3b9fea9b257f5c
SithuBo <sithubo.stb97@gmail.com>
2024-11-15 23:53:29 +0700

    [add] validate agreement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :community_admins, inverse_of: :user
   has_many :community_users, inverse_of: :user
 
+  validates :agreement, acceptance: { allow_nil: false, accept: [true, 'true', '1'] }, on: :create
+
   devise :database_authenticatable, stretches: 13
 
   validates :email, uniqueness: true, presence: true
