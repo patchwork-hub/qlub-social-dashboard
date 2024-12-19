95ff26fc509cb98ed86d0115f71b089abdb9b374
SithuBo <sithubo.stb97@gmail.com>
2024-12-19 23:15:29 +0700

    [remove] commented-out code and temporarily hide visibility options in community preview

--- app/javascript/custom_js/community_preview.js
+++ app/javascript/custom_js/community_preview.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const buttons = document.querySelectorAll('.btn-group-toggle .btn');
-  
-  buttons.forEach(button => {
-    button.addEventListener('click', function() {
-      buttons.forEach(btn => btn.classList.remove('selected'));
-      
-      this.classList.add('selected');
-      
-      const radio = this.querySelector('input[type="radio"]');
-      if (radio) {
-        radio.checked = true;
-      }
-    });
-  });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   const buttons = document.querySelectorAll('.btn-group-toggle .btn');
+
+//   buttons.forEach(button => {
+//     button.addEventListener('click', function() {
+//       buttons.forEach(btn => btn.classList.remove('selected'));
+
+//       this.classList.add('selected');
+
+//       const radio = this.querySelector('input[type="radio"]');
+//       if (radio) {
+//         radio.checked = true;
+//       }
+//     });
+//   });
+// });
