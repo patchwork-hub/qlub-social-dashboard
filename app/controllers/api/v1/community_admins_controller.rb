d0c056698a18e961e0210b597b378736c363f1b0
SithuBo <sithubo.stb97@gmail.com>
2024-12-23 16:02:00 +0700

    Fix boost_bot_accounts_list to conditionally format URL based on channel type

--- app/controllers/api/v1/community_admins_controller.rb
+++ app/controllers/api/v1/community_admins_controller.rb
@@ -8,6 +8,7 @@ module Api
       end
 
       private
+
       def boost_bot_accounts_list
         community_admins = CommunityAdmin.where(is_boost_bot: true)
 
@@ -19,19 +20,20 @@ module Api
 
           channel_type = community.channel_type
           name = community.slug
-          url = "#{name}.channel.org"
+
+          url = community.channel? ? "https://#{name}.channel.org" : ""
+
           account_id = community_admin.account_id
 
           result[name] = {
             account_id: account_id,
             channel_type: channel_type,
-            url: "https://#{url}"
+            url:  url
           }
         end
 
         result
       end
-
     end
   end
 end
