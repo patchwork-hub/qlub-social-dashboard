85bdc94a85ac97a3847adf1dcf8ee7f5d1c58b1f
SithuBo <sithubo.stb97@gmail.com>
2024-12-23 15:46:28 +0700

    Add CommunityAdminsController with boost_bot_accounts action to retrieve boost bot account for feed.channel.org

--- config/routes/api_v1.rb
+++ config/routes/api_v1.rb
@@ -4,7 +4,7 @@ namespace :api, defaults: { format: :json } do
   namespace :v1 do
     resources :accounts
     patch 'api_key/rotate', to: 'api_keys#rotate'
-    
+
     resources :channels, only: [  ] do
       collection do
         get :recommend_channels
@@ -26,6 +26,12 @@ namespace :api, defaults: { format: :json } do
         post :log_out
       end
     end
-    
+
+    resources :community_admins, only: [] do
+      collection do
+        get :boost_bot_accounts
+      end
+    end
+
   end
 end
