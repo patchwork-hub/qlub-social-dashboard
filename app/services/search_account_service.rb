671819b13accaa4b34ea2a7ec154fc78030f671a
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-01-15 00:29:51 +0630

    Putting logs

--- app/services/search_account_service.rb
+++ app/services/search_account_service.rb
@@ -11,7 +11,11 @@ class SearchAccountService
 
   def call
     response = search_mastodon
+    puts "[SearchAccountService] response: #{response}"
+
     accounts = response.parsed_response['accounts']
+    puts "[SearchAccountService] accounts: #{accounts}"
+    puts "[SearchAccountService] find_saved_accounts_with_retry: #{find_saved_accounts_with_retry(accounts)}"
     find_saved_accounts_with_retry(accounts)
   end
 
