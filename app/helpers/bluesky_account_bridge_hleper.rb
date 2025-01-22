b6dbcea9847a333528df7ff57da677885883d885
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-01-22 11:56:11 +0630

    Fix type

--- app/helpers/bluesky_account_bridge_hleper.rb
+++ app/helpers/bluesky_account_bridge_hleper.rb
@@ -35,19 +35,19 @@ module BlueskyAccountBridgeHleper
   def bridged_completely?(community, bridge_info)
     return false if community.nil? || !bridge_info.present?
 
-    bridge_info&.did == community.did_value && bridge_info.handle.eql?("#{community.slug}.channel.org") 
+    bridge_info['did'] == community.did_value && bridge_info['handle'].eql?("#{community.slug}.channel.org") 
   end
 
   def bridged_account_url(community, bridge_info)
     return false if community.nil? || bridge_info.present?
 
-    "https://bsky.app/profile/#{bridge_info&.handle}"
+    "https://bsky.app/profile/#{bridge_info['handle']}"
   end
 
   def bridged_handle(community, bridge_info)
     return false if community.nil? || bridge_info.present?
 
-    bridge_info&.handle
+    bridge_info['handle']
   end
 
 end
