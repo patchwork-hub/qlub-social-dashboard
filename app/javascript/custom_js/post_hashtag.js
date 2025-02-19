964db12edaf56f2d71d6498a90ce03197af003c1
SithuBo <sithubo.stb97@gmail.com>
2025-02-19 16:34:56 +0700

    refactor: implement CUD functionality for post hashtags controller

--- app/javascript/custom_js/post_hashtag.js
+++ app/javascript/custom_js/post_hashtag.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   const editLinks = document.querySelectorAll('.edit-link');
-  
 
   editLinks.forEach(link => {
     link.addEventListener('click', function() {
-      // Get data from the link
       const hashtag = this.getAttribute('data-hashtag');
       const id = this.getAttribute('data-id');
-      
-      // Populate modal fields
+      const community_id = this.getAttribute('data-community-id');
+
+      // Populate the form fields in the modal
       document.getElementById('post_hashtag_id').value = id;
-      document.getElementById('post_hashtag_name').value = '#'+hashtag;
+      document.getElementById('post_hashtag_name').value = hashtag;
+
+      // Update the form action URL dynamically
+      const form = document.querySelector('#postHashtagEditModel form');
+      form.action = `/channels/${community_id}/post_hashtags/${id}`;
     });
   });
 });
