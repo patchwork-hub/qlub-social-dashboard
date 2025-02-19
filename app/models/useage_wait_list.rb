83252e92f9ec90c5124b8b2e7e47a55427fbc56f
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-02-19 16:36:45 +0630

    feat: add wait list functionality with controller and model integration

--- app/models/useage_wait_list.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UseageWaitList < ApplicationRecord
-  self.table_name = 'patchwork_useage_wait_lists'
-  belongs_to :account, class_name: 'Account', foreign_key: 'account_id'
-  belongs_to :wait_list, class_name: 'WaitList', foreign_key: 'wait_list_id'
-end
