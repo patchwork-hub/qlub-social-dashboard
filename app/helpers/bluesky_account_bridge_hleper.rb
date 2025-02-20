faf50d04c603a67a6d69937cc67c51c8d9cf6b31
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-02-20 10:45:22 +0630

    feat: add channel_feeds endpoint and update related controller actions refactor: remove unused account info checks and adjust styles in community form

--- app/helpers/bluesky_account_bridge_hleper.rb
+++ app/helpers/bluesky_account_bridge_hleper.rb
@@ -1,30 +1,4 @@
 module BlueskyAccountBridgeHleper
-  def check_account_info(community, account)
-    error_messages = []
-    error_message = nil
-    
-    if account&.username.blank?
-      error_messages << "username"
-    end
-
-    if account&.display_name.blank?
-      error_messages << "display name"
-    end
-
-    if account&.avatar.blank?
-      error_messages << "avatar"
-    end
-
-    if account&.header.blank?
-      error_messages << "header"
-    end
-
-    if error_messages.any?
-      error_message = "Your account is missing #{error_messages.join(', ')}."
-    end
-    
-    error_message
-  end
 
   def bridged_completely?(community, bridge_info)
     return false if community.nil? || !bridge_info.present?
