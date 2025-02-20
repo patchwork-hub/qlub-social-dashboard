f4cf01b946773c31c9f183385f47730475153e74
SithuBo <sithubo.stb97@gmail.com>
2025-02-20 17:30:47 +0700

    refactor: add BlueskyService for fetching community profiles and update routes for community hashtags

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,154 +1,99 @@
 class CommunitiesController < BaseController
   before_action :authenticate_user!
-  before_action :set_community, except: %i[step1 step1_save index new is_muted]
+  before_action :set_community, except: %i[step1 step1_save index new]
   before_action :initialize_form, only: %i[step1]
   before_action :set_current_step
   before_action :set_content_type, only: %i[step3 step4 step5 step6]
   before_action :set_api_credentials, only: %i[search_contributor step3 step4]
   before_action :fetch_community_admins, only: %i[step4 step6]
-  PER_PAGE = 10
 
+  PER_PAGE = 10
+  COMMUNITY_FILTER_TYPES = { in: 'filter_in', out: 'filter_out' }.freeze
+
+  # Main actions
   def index
-    unless params[:channel_type].present?
-      redirect_to communities_path(request.query_parameters.merge(channel_type: default_channel_type))
-      return
-    end
+    redirect_to communities_path(request.query_parameters.merge(channel_type: default_channel_type)) unless params[:channel_type].present?
     @channel_type = params[:channel_type] || default_channel_type
-    @records = records_filter.get.where(channel_type: @channel_type)
-    @search = records_filter.build_search
+    @records = load_filtered_records(commu_records_filter).where(channel_type: @channel_type)
+    @search = commu_records_filter.build_search
   end
 
   def step1
-    respond_to do |format|
-      format.html
-    end
+    respond_to(&:html)
   end
 
   def step1_save
-    id = form_params[:id].presence
     @community = CommunityPostService.new.call(
       current_user,
-      id: id,
-      name: form_params[:name],
-      slug: form_params[:slug],
-      bio: form_params[:bio],
-      collection_id: form_params[:collection_id],
-      banner_image: form_params[:banner_image],
-      avatar_image: form_params[:avatar_image],
-      logo_image: form_params[:logo_image],
-      community_type_id: CommunityType.first.id,
-      is_recommended: form_params[:is_recommended]
+      form_params.merge(community_type_id: CommunityType.first.id)
     )
-
     if @community.errors.any?
-      @community_form = Form::Community.new(form_params)
-      flash.now[:error] = @community.errors.full_messages
-      render :step1
+      handle_step1_error
     else
-      if current_user.master_admin? || current_user.user_admin?
-        redirect_to step2_community_path(@community)
-      else
-        redirect_to step3_community_path(@community)
-      end
+      redirect_after_step1_save
     end
   end
 
   def step2
-    authorize @community, :step2?
-    @records = load_commu_admin_records
-    @community_admin = CommunityAdmin.new
+    authorize_step(:step2?)
+    @records = load_filtered_records(commu_admin_records_filter)
+    @community_admin = CommunityAdmin.new(patchwork_community_id: @community.id)
     invoke_bridged
   end
 
   def step3
-    @records = load_commu_hashtag_records
+    @records = load_filtered_records(commu_hashtag_records_filter)
     @search = commu_hashtag_records_filter.build_search
     @community_hashtag_form = Form::CommunityHashtag.new(community_id: @community.id)
     @follow_records = load_follow_records
-    @filter_keywords = get_community_filter_keyword('filter_in')
-    @community_filter_keyword = CommunityFilterKeyword.new(
-      patchwork_community_id: @community.id,
-      filter_type: 'filter_in',
-      is_filter_hashtag: false
-    )
+    setup_filter_keywords(COMMUNITY_FILTER_TYPES[:in])
   end
 
   def step4
-    hashtag = load_commu_hashtag_records
-    if hashtag.empty?
-      flash[:error] = "Please add at least one hashtag in the 'Hashtags' section above before proceeding, as hashtags are required to retrieve content."
-      redirect_to step3_community_path
-    end
-    @muted_accounts = get_muted_accounts
-    @community_post_type = @community.community_post_type ||
-                          CommunityPostType.new(patchwork_community_id: @community.id)
-    @filter_keywords = get_community_filter_keyword('filter_out')
-    @community_filter_keyword = CommunityFilterKeyword.new(
-      patchwork_community_id: @community.id,
-      filter_type: 'filter_out'
-    )
+    verify_hashtags_presence
+    @muted_accounts = load_muted_accounts
+    @community_post_type = @community.community_post_type || new_community_post_type
+    setup_filter_keywords(COMMUNITY_FILTER_TYPES[:out])
   end
 
   def step5
-    authorize @community, :step5?
+    authorize_step(:step5?)
     @form_post_hashtag = Form::PostHashtag.new
-    @records = load_post_hashtag_records
+    @records = load_filtered_records(post_hashtag_records_filter)
     @search = post_hashtag_records_filter.build_search
-    respond_to do |format|
-      format.html
-    end
+    respond_to(&:html)
   end
 
   def step6
-    authorize @community, :step6?
-    @admin = Account.where(id: get_community_admin_id).first
+    authorize_step(:step6?)
+    @admin = Account.find_by(id: admin_account_id)
   end
 
   def manage_additional_information
     authorize @community, :manage_additional_information?
-
-    if params[:community].present?
-      if @community.update(community_params)
-        respond_to do |format|
-          format.html
-        end
-      else
-        flash[:error] = @community.errors.full_messages.join(", ")
-        redirect_to step6_community_path
-      end
-    end
+    update_additional_information
   end
 
   def set_visibility
-    visibility = params.dig(:community, :visibility).presence || 'public_access'
-    if @community.update(visibility: visibility)
-      # admin_email = User.where(account_id: get_community_admin_id)
-      # DashboardMailer.channel_created(@community, admin_email).deliver_now
-      if @community.channel?
-        CreateCommunityInstanceDataJob.perform_later(@community.id, @community.slug) if ENV['ALLOW_CHANNELS_CREATION'] == 'true'
-        redirect_to communities_path(channel_type: 'channel')
-      else
-        redirect_to communities_path(channel_type: 'channel_feed')
-      end
+    if @community.update(visibility: visibility_param)
+      handle_successful_visibility_update
     else
-      if @community.channel?
-        render :step6
-      else
-        render :step4
-      end
+      handle_failed_visibility_update
     end
   end
 
   def new
-    respond_to do |format|
-      format.html
-    end
+    respond_to(&:html)
   end
 
+  # Search/mute actions
   def search_contributor
-    query = params[:query]
-
-    result = ContributorSearchService.new(query, url: @api_base_url, token: @token, account_id: get_community_admin_id).call
+    result = ContributorSearchService.new(
+      params[:query],
+      url: @api_base_url,
+      token: @token,
+      account_id: get_community_admin_id
+    ).call
 
     if result.any?
       render json: { 'accounts' => result }
@@ -158,33 +103,21 @@ class CommunitiesController < BaseController
   end
 
   def mute_contributor
-    target_account_id = params[:account_id]
-    admin_account_id = get_community_admin_id
-    mute = params[:mute]
-    if mute
-      Mute.find_or_create_by!(account_id: admin_account_id, target_account_id: target_account_id, hide_notifications: true)
-    else
-      Mute.find_by(account_id: admin_account_id, target_account_id: target_account_id)&.destroy
-    end
-
+    handle_mute_action(params[:mute])
     render json: { success: true }
   end
 
   def unmute_contributor
-    target_account_id = params[:account_id]
-    Mute.find_by(account_id: get_community_admin_id, target_account_id: target_account_id)&.destroy
-
+    unmute_target_account
     redirect_to step4_community_path
   end
 
-  def is_muted
-    target_account_id = params[:account_id]
-    is_muted = Mute.exists?(account_id: get_community_admin_id, target_account_id: target_account_id)
-
-    render json: { is_muted: is_muted }
-  end
-
   private
+
+  # Before actions
+  def set_content_type
+    @content_type = @community.content_type
+  end
 
   def initialize_form
     if params[:id].present? || (params[:form_community] && params[:form_community][:id].present?)
@@ -192,7 +125,6 @@ class CommunitiesController < BaseController
       @community = Community.find_by(id: id)
       if @community.present?
         authorize @community, :initialize_form?
-
         form_data = {
           id: @community.id,
           name: @community.name,
@@ -220,16 +152,13 @@ class CommunitiesController < BaseController
     @community_admins = CommunityAdmin.where(patchwork_community_id: @community.id)
   end
 
-  def post_hashtag_params
-    params.require(:form_post_hashtag).permit(:hashtag1, :hashtag2, :hashtag3, :community_id)
-  end
-
-  def community_hashtag_params
-    params.require(:form_community_hashtag).permit(:community_id, :hashtag)
-  end
-
+  # Parameter handling
   def form_params
-    params.require(:form_community).permit(:id, :name, :slug, :collection_id, :bio, :banner_image, :avatar_image, :logo_image, :community_type_id, :is_recommended)
+    params.require(:form_community).permit(
+      :id, :name, :slug, :collection_id, :bio,
+      :banner_image, :avatar_image, :logo_image,
+      :community_type_id, :is_recommended
+    )
   end
 
   def community_params
@@ -241,71 +170,152 @@ class CommunitiesController < BaseController
     )
   end
 
-  def records_filter
-    Filter::Community.new(params, current_user)
+  # Setup methods
+
+  # Action handlers
+
+  def handle_step1_error
+    @community_form = Form::Community.new(form_params)
+    flash.now[:error] = @community.errors.full_messages
+    render :step1
   end
 
-  def load_commu_admin_records
-    commu_admin_records_filter.get
+  def redirect_after_step1_save
+    path = (current_user.master_admin? || current_user.user_admin?) ? :step2 : :step3
+    redirect_to send("#{path}_community_path", @community)
   end
 
-  def load_commu_hashtag_records
-    commu_hashtag_records_filter.get
+  def update_additional_information
+    if params[:community].blank?
+      @community.errors.add(:base, "Missing additional information")
+      return handle_update_error(step6_community_path)
+    end
+
+    if @community.update(community_params)
+      respond_to(&:html)
+    else
+      handle_update_error(step6_community_path)
+    end
   end
 
-  def load_post_hashtag_records
-    post_hashtag_records_filter.get
-  end
-
-  def load_commu_follower_records
-    commu_follower_filter.get
+  # Filter and load methods
+  def load_filtered_records(filter)
+    filter.get
   end
 
   def load_follow_records
-    account_id = get_community_admin_id
-    follow_ids = Follow.where(account_id: account_id).pluck(:target_account_id)
-    follow_request_ids = FollowRequest.where(account_id: account_id).pluck(:target_account_id)
-    total_follows_ids = follow_ids + follow_request_ids
-    Account.where(id: total_follows_ids).page(params[:page]).per(PER_PAGE)
+    account_ids = Follow.where(account_id: admin_account_id).pluck(:target_account_id) +
+                  FollowRequest.where(account_id: admin_account_id).pluck(:target_account_id)
+    paginated_records(Account.where(id: account_ids))
   end
 
-  def commu_follower_filter
-    @follower_filter = Filter::Account.new(params)
+  def load_muted_accounts
+    muted_ids = Mute.where(account_id: admin_account_id).pluck(:target_account_id)
+    paginated_records(Account.where(id: muted_ids))
+  end
+
+  # Authorization and verification
+  def authorize_step(policy_method)
+    authorize @community, policy_method
+  end
+
+  def verify_hashtags_presence
+    return if load_filtered_records(commu_hashtag_records_filter).any?
+    flash[:error] = "Please add at least one hashtag in the 'Hashtags' section above before proceeding, as hashtags are required to retrieve content."
+    redirect_to step3_community_path
+  end
+
+  # Helper methods
+  def setup_filter_keywords(filter_type)
+    @filter_keywords = community_filter_keywords(filter_type)
+    @community_filter_keyword = CommunityFilterKeyword.new(
+      patchwork_community_id: @community.id,
+      filter_type: filter_type,
+      is_filter_hashtag: filter_type == COMMUNITY_FILTER_TYPES[:in] ? false : nil
+    )
+  end
+
+  def community_filter_keywords(filter_type)
+    paginated_records(CommunityFilterKeyword.where(patchwork_community_id: @community.id, filter_type: filter_type))
+  end
+
+  def paginated_records(scope)
+    scope.page(params[:page]).per(PER_PAGE)
+  end
+
+  def admin_account_id
+    @admin_account_id ||= get_community_admin_id
+  end
+
+  # Mute handling
+  def handle_mute_action(mute)
+    if mute
+      Mute.find_or_create_by!(
+        account_id: admin_account_id,
+        target_account_id: params[:account_id],
+        hide_notifications: true
+      )
+    else
+      Mute.find_by(account_id: admin_account_id, target_account_id: params[:account_id])&.destroy
+    end
+  end
+
+  def unmute_target_account
+    Mute.find_by(account_id: admin_account_id, target_account_id: params[:account_id])&.destroy
+  end
+
+  # Visibility handling
+  def visibility_param
+    params.dig(:community, :visibility).presence || 'public_access'
+  end
+
+  def handle_successful_visibility_update
+    # admin_email = User.where(account_id: get_community_admin_id)
+    # DashboardMailer.channel_created(@community, admin_email).deliver_now
+    if @community.channel?
+      CreateCommunityInstanceDataJob.perform_later(@community.id, @community.slug) if channels_allowed?
+      redirect_to communities_path(channel_type: 'channel')
+    else
+      redirect_to communities_path(channel_type: 'channel_feed')
+    end
+  end
+
+  def handle_failed_visibility_update
+    render @community.channel? ? :step6 : :step4
+  end
+
+  def channels_allowed?
+    ENV['ALLOW_CHANNELS_CREATION'] == 'true'
+  end
+
+  # Filter initializers
+  def commu_records_filter
+    Filter::Community.new(params, current_user)
   end
 
   def commu_admin_records_filter
-    params[:q] = { patchwork_community_id_eq: @community.id }
-    @filter = Filter::CommunityAdmin.new(params)
-  end
-
-  def post_hashtag_records_filter
-    params[:q] = { patchwork_community_id_eq: @community.id }
-    @filter = Filter::PostHashtag.new(params)
+    initialize_filter(Filter::CommunityAdmin, { patchwork_community_id_eq: @community.id })
   end
 
   def commu_hashtag_records_filter
-    params[:q] = { patchwork_community_id_eq: @community.id }
-    Filter::CommunityHashtag.new(params)
+    initialize_filter(Filter::CommunityHashtag, { patchwork_community_id_eq: @community.id })
   end
 
-  def get_community_filter_keyword(filter_type)
-    CommunityFilterKeyword.where(patchwork_community_id: @community.id, filter_type: filter_type).page(params[:page]).per(PER_PAGE)
+  def post_hashtag_records_filter
+    initialize_filter(Filter::PostHashtag, { patchwork_community_id_eq: @community.id })
   end
 
-  def get_muted_accounts
-    admin_account_id = get_community_admin_id
-    muted_account_ids = Mute.where(account_id: admin_account_id).pluck(:target_account_id)
-    Account.where(id: muted_account_ids).page(params[:page]).per(PER_PAGE)
+  def initialize_filter(filter_class, query_params)
+    params[:q] = query_params
+    filter_class.new(params)
   end
 
+  # Common methods
   def set_community
     @community = Community.find(params[:id])
     authorize @community, :initialize_form?
-    raise ActiveRecord::RecordNotFound unless @community
-  end
-
-  def set_content_type
-    @content_type = @community.content_type
+  rescue ActiveRecord::RecordNotFound
+    not_found
   end
 
   def default_channel_type
@@ -313,24 +323,20 @@ class CommunitiesController < BaseController
   end
 
   def set_current_step
-    @current_step = action_name.match(/\d+/).to_s.to_i || 1
+    @current_step = action_name[/\d+/].to_i || 1
   end
 
+  def new_community_post_type
+    CommunityPostType.new(patchwork_community_id: @community.id)
+  end
+
+  def handle_update_error(redirect_path)
+    flash[:error] = @community.errors.full_messages.join(", ")
+    redirect_to redirect_path
+  end
+
+  # Bluesky integration
   def invoke_bridged
-    @account = CommunityAdmin.find_by(patchwork_community_id: @community.id)&.account
-    @bluesky_info = fetch_bluesky_account
-  end
-
-  def fetch_bluesky_account
-    url = "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=#{@community&.slug}.channel.org"
-    response = HTTParty.get(url)
-    if response.code == 200
-      results = JSON.parse(response.body)
-      Rails.logger.info("Fetched bluesky account: #{results}")
-      results
-    else
-      Rails.logger.error("Failed to fetch bluesky account: #{@community&.slug} => #{response.body}")
-      {}
-    end
+    @bluesky_info = BlueskyService.new(@community).fetch_bluesky_account
   end
 end
