a4a9461980285587f26372707d743da013360e56
SithuBo <sithubo.stb97@gmail.com>
2025-02-27 18:27:46 +0700

    fix: enhance error handling and retry logic in fetch_bluesky_account method

--- app/services/bluesky_service.rb
+++ app/services/bluesky_service.rb
@@ -5,14 +5,25 @@ class BlueskyService
   end
 
   def fetch_bluesky_account
-    response = HTTParty.get(@url)
-    if response.code == 200
-      results = JSON.parse(response.body)
-      Rails.logger.info("Fetched bluesky account: #{results}")
-      results
-    else
+    retries = 2
+    begin
+      response = HTTParty.get(@url)
+
+      return JSON.parse(response.body) if response.code == 200
+
       Rails.logger.error("Failed to fetch bluesky account: #{@community&.slug} => #{response.body}")
+      {}
+    rescue Socket::ResolutionError, SocketError => e
+      if (retries -= 1) > 0
+        sleep 1
+        retry
+      end
+      Rails.logger.error("Network error after retries: #{e.message}")
+      {}
+    rescue StandardError => e
+      Rails.logger.error("Unexpected error: #{e.message}")
       {}
     end
   end
+
 end
