3e2bb7e5aaa7bd3799c7e14e47756e60823538d9
SithuBo <sithubo.stb97@gmail.com>
2025-05-14 13:01:38 +0700

    change max limit

--- app/models/community_rule.rb
+++ app/models/community_rule.rb
@@ -19,5 +19,5 @@
 class CommunityRule < ApplicationRecord
   self.table_name = 'patchwork_community_rules'
   belongs_to :community, class_name: 'Community', foreign_key: 'patchwork_community_id'
-  validates :rule, presence: true, length: { maximum: 255 }
+  validates :rule, presence: true, length: { maximum: 500 }
 end
