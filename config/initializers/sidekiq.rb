752b99f26eae799d34bfbb434348fc86d4d10930
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-04 17:19:47 +0630

    fix:sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,39 +1,23 @@
 require 'sidekiq'
 require 'sidekiq/web'
 require 'sidekiq-scheduler'
-
-# redis_url = if ENV['REDIS_PASSWORD'].present?
-#   "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
-# else
-#   "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
-# end
-
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}",
-      password: ENV['REDIS_PASSWORD']
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}",
-      password: ENV['REDIS_PASSWORD']
-    }
-  end
+require 'uri'
+ 
+redis_url = if ENV['REDIS_PASSWORD'].present?
+  encoded_password = URI.encode_www_form_component(ENV['REDIS_PASSWORD'])
+  "redis://:#{encoded_password}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
+else
+  "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
 end
-
-if Rails.env.development?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
-    }
-  end
+ 
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: redis_url
+  }
+end
+ 
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: redis_url
+  }
 end
