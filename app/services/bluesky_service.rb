eed943c7d3c11e8374d1bb7ca6120cbdecfc811c
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-17 11:12:53 +0630

    fix:bluesky account

--- app/services/bluesky_service.rb
+++ app/services/bluesky_service.rb
@@ -1,27 +1,45 @@
 class BlueskyService
+
   def initialize(community)
-    @community = community
-    @url = "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=#{@community&.slug}.channel.org"
+    admin_account = community&.community_admins.where(is_boost_bot: true).last
+    account = Account.find_by(id: admin_account&.account_id)
+
+    @urls = []
+    @urls << "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=#{community&.slug}.channel.org" if community&.slug 
+    @urls << "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=#{account&.username}.channel.org" if account&.username 
   end
 
   def fetch_bluesky_account
-    retries = 2
+    @urls.each do |url|
+      result = fetch_with_retries(url)
+      return result unless result.empty?
+    end
+
+    {}
+  end
+
+  private
+
+  def fetch_with_retries(url, retries = 2)
     begin
-      response = HTTParty.get(@url)
+      response = HTTParty.get(url)
 
-      return JSON.parse(response.body) if response.code == 200
-
-      Rails.logger.error("Failed to fetch bluesky account: #{@community&.slug} => #{response.body}")
-      {}
+      if response.code == 200
+        return JSON.parse(response.body)
+      else
+        Rails.logger.error("Failed to fetch bluesky account from #{url}: #{response.body}")
+        return {}
+      end
     rescue Socket::ResolutionError, SocketError => e
       if (retries -= 1) > 0
         sleep 1
         retry
+      else
+        Rails.logger.error("Network error after retries for #{url}: #{e.message}")
+        return {}
       end
-      Rails.logger.error("Network error after retries: #{e.message}")
-      {}
     rescue StandardError => e
-      Rails.logger.error("Unexpected error: #{e.message}")
+      Rails.logger.error("Unexpected error fetching #{url}: #{e.message}")
       {}
     end
   end
