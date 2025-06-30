0765abd3fafe0935f5af32d552b4b65e338787f7
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2025-06-30 11:21:53 +0630

    fix: desc banner image, falsh msg on create admin

--- app/helpers/step_helper.rb
+++ app/helpers/step_helper.rb
@@ -9,7 +9,7 @@ module StepHelper
 
     if master_admin? && is_channel
       steps << { step: 0, display: 1, title: 'Choose community type', description: 'Select the type of community you want to create.' }
-      steps << { step: 1, display: 2, title: 'Community Information', description: 'Set up the basic details of your community.' }
+      steps << { step: 1, display: 2, title: 'Community information', description: 'Set up the basic details of your community.' }
       steps << { step: 2, display: 3, title: 'Admin and public feed details', description: 'Create admin accounts for your community.' }
       if is_custom_channel
         steps << { step: 3, display: 4, title: 'Add content', description: 'Populate your channel with content from across the New Social network. Here you can define rules that specify what content is included in your community.' }
@@ -31,7 +31,7 @@ module StepHelper
       steps << { step: 4, display: 4, title: 'Filter content', description: 'Filter content from the wider network to ensure your newsmast channel stays relevant.' }
     elsif organisation_admin? && is_channel
       steps << { step: 0, display: 1, title: 'Choose Channel Type', description: 'Select the type of channel you want to create.' }
-      steps << { step: 1, display: 2, title: 'Community Information', description: 'Set up the basic details of your channel.' }
+      steps << { step: 1, display: 2, title: 'Community information', description: 'Set up the basic details of your channel.' }
       if is_custom_channel
         steps << { step: 3, display: 3, title: 'Add content', description: 'Populate your channel with content from across the New Social network. Here you can define rules that specify what content is included in your channel.' }
         steps << { step: 4, display: 4, title: 'Filter content', description: 'Filter content from the wider network to ensure your channel stays relevant.' }
