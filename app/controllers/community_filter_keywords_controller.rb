fa8208c452a37ea99b62ed2722cd83d86ee13041
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-07-29 13:58:10 +0630

    feat:global filter keywords

--- app/controllers/community_filter_keywords_controller.rb
+++ app/controllers/community_filter_keywords_controller.rb
@@ -1,6 +1,12 @@
 class CommunityFilterKeywordsController < ApplicationController
   before_action :set_community_filter_keyword, only: [:edit, :update, :destroy]
 
+  PER_PAGE = 10
+  def index
+    @search = fetch_keywords
+    @keywords = @search.result.page(params[:page]).per(PER_PAGE)
+  end
+
   def create
     @community_filter_keyword = CommunityFilterKeyword.new(community_filter_keyword_params)
     patchwork_community_id = params.dig(:community_filter_keyword, :patchwork_community_id)
@@ -45,5 +51,25 @@ class CommunityFilterKeywordsController < ApplicationController
 
   def determine_redirect_path(patchwork_community_id, filter_type)
     filter_type == 'filter_in' ? step3_community_path(id: patchwork_community_id) : step4_community_path(id: patchwork_community_id)
+  end
+
+  def fetch_keywords
+    filter_type = params[:filter_type].to_s.strip
+    filter_type = 'filter_out' unless %w[filter_out filter_in].include?(filter_type)
+
+    begin
+      community_filter_keywords = CommunityFilterKeyword
+                                    .select(:id, :keyword, :filter_type, :is_filter_hashtag, :patchwork_community_id)
+                                    .where(
+                                      patchwork_community_id: params[:community_id].presence,
+                                      filter_type: filter_type
+                                    )
+                                    .order(:keyword).ransack(params[:q])
+      community_filter_keywords
+    rescue ActiveRecord::ActiveRecordError => e
+      Rails.logger.error("Error fetching community filter keywords: #{e.message}")
+      community_filter_keywords = []
+      flash[:error] = 'An error occurred while fetching filter keywords. Please try again later.'
+    end
   end
 end
