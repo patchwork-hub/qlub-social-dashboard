cf0e0eb42a05135f5642099d68f14749251b04d3
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-08-13 14:24:04 +0700

    Check the header for auth

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class ChannelsController < ApiController
       skip_before_action :verify_key!
-      before_action :check_authorization_header, only: [:channel_detail, :channel_feeds, :newsmast_channels, :my_channel, :mo_me_channels]
+      before_action :check_authorization_header, only: [:channel_detail, :channel_feeds, :newsmast_channels, :my_channel, :mo_me_channels, :patchwork_demo_channels]
       before_action :set_channel, only: [:channel_detail, :channel_feeds]
 
       DEFAULT_MO_ME_CHANNELS = [
