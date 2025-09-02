d7cf998437bec43eaa23f31342f89c3562301054
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-09-02 20:06:18 +0630

    Wrap migration changes in safety_assured block for released_date column

--- db/migrate/20250902092150_add_released_date_to_app_versions.rb
+++ db/migrate/20250902092150_add_released_date_to_app_versions.rb
@@ -1,5 +1,7 @@
 class AddReleasedDateToAppVersions < ActiveRecord::Migration[7.1]
   def change
-    add_column :patchwork_app_version_histories, :released_date, :datetime, null: true, default: -> { 'CURRENT_TIMESTAMP' } unless column_exists?(:patchwork_app_version_histories, :released_date)
+    safety_assured do
+      add_column :patchwork_app_version_histories, :released_date, :datetime, null: true, default: -> { 'CURRENT_TIMESTAMP' } unless column_exists?(:patchwork_app_version_histories, :released_date)
+    end
   end
 end
--- db/migrate/20250902101613_change_released_date_to_nullable_in_app_version_histories.rb
+++ db/migrate/20250902101613_change_released_date_to_nullable_in_app_version_histories.rb
@@ -1,5 +1,7 @@
 class ChangeReleasedDateToNullableInAppVersionHistories < ActiveRecord::Migration[7.1]
   def change
-    change_column :patchwork_app_version_histories, :released_date, :datetime, null: true, default: -> { 'CURRENT_TIMESTAMP' }
+    safety_assured do
+      change_column :patchwork_app_version_histories, :released_date, :datetime, null: true, default: -> { 'CURRENT_TIMESTAMP' }
+    end
   end
 end
