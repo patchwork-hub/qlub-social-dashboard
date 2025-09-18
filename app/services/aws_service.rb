28ff1dabf1454263b2eb6302830cb1092999c518
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-09-18 11:39:47 +0630

    Refactor AWS service configuration to avoid setting global AWS config and utilize route53_client method in services

--- app/services/aws_service.rb
+++ app/services/aws_service.rb
@@ -4,7 +4,12 @@ require 'aws-sdk-route53'
 
 class AwsService
   def self.configure_aws
-    Aws.config.update(
+    # No longer set global AWS config - let Paperclip use its own S3 credentials
+    # Route53 clients will use their own credentials separately
+  end
+
+  def self.route53_client
+    Aws::Route53::Client.new(
       region: ENV['AWS_DNS_REGION'],
       credentials: Aws::Credentials.new(
         ENV['AWS_ACCESS_DNS_RESOLVE_ID'],
